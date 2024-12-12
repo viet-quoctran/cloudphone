c99a07f2550361ecedab50d9eda3641a3931edea
viet-quoctran <viettqgcd18575@fpt.edu.vn>
2024-12-12 17:19:51 +0700

    Master

---
+++ app/Http/Kernel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        // ...
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            // ...
+        ],
+
+        'api' => [
+            // ...
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        // Add your middleware here, e.g.:
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+    ];
+}
